eb06b34c23260425608abdc4b381297d3c033eff
Pangundian Siagian <147624671+PangundianSiagian@users.noreply.github.com>
2024-09-03 13:52:46 +0700

    Pertemuan 1

---
+++ 23038_PangundianSiagian_Praktikum_Tugas/src/main/java/com/mycompany/_pangundiansiagian_praktikum_tugas/App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany._pangundiansiagian_praktikum_tugas;
+
+/**
+ *
+ * @author Asus
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
